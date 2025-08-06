045a9c8d2f350adaa584a1327388e6665e488ba4
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-08-06 05:01:21 -0700

    Register book models in Django admin
    Added registration of Book, Author, BookAuthor, and BookReview models to the Django admin site for easier management. Updated README to include a reference to an image, and made minor formatting changes in models.py.

--- book/models.py
+++ book/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     isbn = models.CharField(max_length=17)
-
+    
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
